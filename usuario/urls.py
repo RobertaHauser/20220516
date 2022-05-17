8ce34be35018b1a6aafbaa15b9af8ccc4abd2f6d
Roberta Hauser <roberta.hauser@gmail.com>
2022-05-17 06:18:19 -0300

    Tutorial Rafael Zotesso Part24

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+from .views import UsuarioCreate, index
+
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('', index, name='index'),
     path('login/', auth_views.LoginView.as_view(
             template_name='login.html'
         ), name="login"),
-    #path('registrar/', UsuarioCreate.as_view(), name='registrar'),
+    path('registrar/', UsuarioCreate.as_view(), name='registrar'),
+
 ]
